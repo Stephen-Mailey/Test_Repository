d39fbcaf8d0630e9171da2656764c5868df78517
unknown <L00137623@student.lyit.ie>
2021-01-29 16:32:31 +0000

    Added HelloLYIT.java - Second commit

---
+++ HelloLYIT.java
@@ -0,0 +1,10 @@
+public class HelloLYIT {
+	public static void(String{} args) {
+		//Prints "Hello, LYIT DevOps course from Stephen" as the output.
+		
+		System.out.println("Hello, LYIT DevOps course from Stephen");
+	}
+}	
+	
+  
+  
